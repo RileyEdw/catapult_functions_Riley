b220e2ec31fa0e855753bfb561773da4b6cb488b
RileyEdw <edwards.riley777@gmail.com>
2025-06-18 13:20:34 -0500

    frog

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ let button = document.querySelector(".password-check");
 // Write a function to test if the password is correct
 // The function should take in the text of the password
 // as an input, and return true if the password is correct
-
+function check(input){
+    if(input == "frog"){return true}
+    else{return false}
+}
 
 button.addEventListener("click", function() {
 
@@ -12,7 +15,7 @@ button.addEventListener("click", function() {
 
     // 2 - Call your function here, store the result in a variable
     // and use the variable to determine which message to display
-    if () {
+    if (check(password)) {
         document.querySelector(".yes").style = "display:block";
         document.querySelector(".denied").style = "display:none";
     } else {
